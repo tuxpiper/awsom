1f2969d5c93abcfa6590abe8d5b324c7d08773e6
David Losada <david@tuxpiper.com>
2013-05-17 21:29:23 +0200

    Pypi packaging

--- setup.py
+++ setup.py
@@ -1,12 +1,25 @@
 from setuptools import setup, find_packages
-import sys
 
 setup(
     name = "awsom",
-    version = "0.0.1",
+    description = ("Browse your AWS resources using a hierarchical object model"),
+    author = "David Losada Carballo",
+    author_email = "david@tuxpiper.com",
+    version = "0.0.2",
     packages = find_packages(),
-    scripts = ['awsom.py'],
+    license = 'MIT',
+    keywords = "aws internet cloud",
+    long_description = open('README.md').read(),
+    url = "http://github.com/tuxpiper/awsom",
+    classifiers=[
+       "Development Status :: 1 - Planning",
+       "Topic :: System",
+       "Environment :: Console",
+       "Intended Audience :: System Administrators",
+       "License :: OSI Approved :: MIT License",
+       "Programming Language :: Python"
+    ],
     
-    # We use boto for interacting with AWS services
+    # We use boto for interacting with AWS services, YAML for storing configuration
     install_requires = ['boto>=2.7.0', 'PyYAML==3.10']    
 )
